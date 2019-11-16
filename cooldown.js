59cc564707e31ebefc7cbc15ef1e86d51633f373
koko <51544507+koko-0012@users.noreply.github.com>
2019-11-16 00:54:44 +0000

    Update and rename rep.js to cooldown.js

---
+++ cooldown.js
@@ -0,0 +1,43 @@
+const { RichEmbed, Collection } = require("discord.js")
+const cooldown = new Collection();
+const { convertMS } = require("discordutility");
+let time = 18000000;
+
+/*
+Created by koko
+Rep system
+Using mysql
+With cooldown
+*/
+
+
+/*
+converted.d = days
+converted.h = hours
+converted.m = minutes
+converted.s = seconds
+converted.ms = milliseconds
+*/
+
+
+module.exports = { 
+  config: {
+      name: "Command",
+  },
+run: (bot, message, args, connection) => {
+  message.delete(); //delete the message like 
+    
+    if (cooldown.has(message.author.id)) { // if user on cooldown
+      
+      const timeLeft = cooldown.get(message.author.id) - Date.now(); 
+      const converted = convertMS(timeLeft); // Changes the ms to time
+      //add message here if 
+    
+    } else {
+    
+    cooldown.set(message.author.id, Date.now() + time); // <- saves the time 
+    setTimeout(() => cooldown.delete(), time) // <- I don't remember what it does but it's needed
+    })
+  }
+}
+}
